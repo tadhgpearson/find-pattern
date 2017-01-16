5c838c19f39b6394c61c426d24595d8c6a53e32c
Tadhg Pearson <tadhg.pearson@amadeus.com>
2017-01-16 11:49:48 +0000

    Read input from the console - this requires NPM

--- find-pattern.js
+++ find-pattern.js
@@ -1,7 +1,8 @@
+module.exports = findPattern;
+
 /* Checks for the pattern that exists in the input - including patterns that may be cut short by a terminator.
    Returns true if the provided pattern is the smallest pattern. Otherwise, returns false */
 function isPattern(pattern, input){
-	/* If the two strings are the same, then obviously this matches */
 	if(pattern == input){
 		return true;
 	}
@@ -27,7 +28,7 @@ function findPattern(input){
 		return input;
 	}
 	else{
-		for(var i=0; i<input.length; i++){
+		for(var i=0; i < input.length; i++){
 			var pattern = input.substring(0, i+1);
 			if(isPattern(pattern, input)){
 				return pattern;
@@ -37,5 +38,13 @@ function findPattern(input){
 	}
 }
 
-var input = process.argv[2]; /* TODO read from stdin */
-console.log(findPattern(input));
+var input = process.argv[2];
+
+if(!input){
+	console.log("Find Pattern: Finds the shortest repeating pattern in the input string.");
+	console.log("For example: Given toto, the shortest pattern is to.");
+	var readline = require('readline-sync');
+	input = readline.question("Provide an input to check for patterns:>")
+}
+ 
+console.log(input + " -> " +findPattern(input));
